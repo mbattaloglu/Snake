323cb9b520c58ea9b07e6bb1b345509cd0b2cc09
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-13 05:11:25 +0300

    Growing

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
     private Vector2 direction = Vector2.right;
+
+    private List<Transform> segments;
+
+    public Transform segmentPrefab;
+
+    private void Start()
+    {
+        this.segments = new List<Transform>();
+        this.segments.Add(this.transform);
+    }
 
     private void Update()
     {
@@ -26,10 +37,29 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
         transform.position = new Vector3(
             Mathf.Round(transform.position.x + direction.x),
             Mathf.Round(transform.position.y + direction.y),
             0f
         );
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            Grow();
+        }
     }
 }
