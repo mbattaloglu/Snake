c510a21331a7b759bd66151f6b03061ab2ebf27d
mbattaloglu <mmbattaloglu@gmail.com>
2023-04-13 04:59:27 +0300

    Food

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D gridArea;
+
+    private void Start()
     {
-        
+        RandomizePosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RandomizePosition()
     {
-        
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            RandomizePosition();
+        }
     }
 }
